573222b176948a967cacb4b1e905728e46393a99
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2023-05-05 14:30:28 +0700

    Create yt playlist calculator

---
+++ yt-playlist-calculator.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        YouTube Playlist Calculator
+// @version     1.0.0
+// @author      sapondanaisriwan
+// @namespace   https://github.com/sapondanaisriwan/Youtube-Playlist-Calculator
+// @description Get the total length/duration of a YouTube playlist.
+// @match       https://www.youtube.com/*
+// @grant       none
+// @license     MIT
+// @homepageURL https://github.com/sapondanaisriwan/Youtube-Playlist-Calculator
+// @updateURL   https://github.com/sapondanaisriwan/Youtube-Playlist-Calculator/raw/main/yt-playlist-calculator.user.js
+// @supportURL  https://github.com/sapondanaisriwan/Youtube-Playlist-Calculator/issues
+// @icon        https://i.imgur.com/I9uDrsq.png
+// ==/UserScript==
+
+/*
+If you want to submit a bug or request a feature please report via github issue. Since I receive so many emails, I can't reply to them all.
+Contact: sapondanaisriwan@gmail.com
+Support me: https://ko-fi.com/sapondanaisriwan 
+Support me: https://ko-fi.com/sapondanaisriwan
+Support me: https://ko-fi.com/sapondanaisriwan
+Support me: https://ko-fi.com/sapondanaisriwan
+Support me: https://ko-fi.com/sapondanaisriwan
+*/
+
+"use strict";
+
